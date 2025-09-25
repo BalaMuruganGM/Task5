5fc1692f1eff3fe273d0f98a937e92f382ab5d77
BalaMuruganGM <balagm56@gmail.com>
2025-09-25 07:49:51 -1200

    Remove

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Form from './Component/Form';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/BalaMuruganGM/task9">
-      <Routes>
-        <Route path="/" element={<Form />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+        <Form />
+  )
+}
 
 export default App;
